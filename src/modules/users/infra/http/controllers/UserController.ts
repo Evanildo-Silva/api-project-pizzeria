df70cf7d8a1dc039df3009bf72c563cd13b00e0d
evanildo <evanildo.sf91@gmail.com>
2023-11-08 15:45:52 -0300

    feat: add show method in user controller

--- src/modules/users/infra/http/controllers/UserController.ts
+++ src/modules/users/infra/http/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import CreateUserService from "@modules/users/services/CreateUserService";
+import ShowUserService from "@modules/users/services/ShowUserService";
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
@@ -13,6 +14,16 @@ class UserController {
       email,
       password,
     });
+
+    return response.json(user);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showUser = container.resolve(ShowUserService);
+
+    const user = await showUser.execute({ id });
 
     return response.json(user);
   }
