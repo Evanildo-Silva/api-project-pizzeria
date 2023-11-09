d5198e82e7f7875ef90303521ddb4dfc3d7358ff
evanildo <evanildo.sf91@gmail.com>
2023-11-09 19:25:34 -0300

    feat: session controller

---
+++ src/modules/users/infra/http/controllers/SessionController.ts
@@ -0,0 +1,20 @@
+import CreateSessionService from "@modules/users/services/CreateSessionSession";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+class SessionController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const createSession = container.resolve(CreateSessionService);
+
+    const user = await createSession.execute({
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+}
+
+export default SessionController;
