ef4ee3733ae3c9aeb9461af99347527f27543b04
evanildo <evanildo.sf91@gmail.com>
2023-11-09 19:08:18 -0300

    refactor: remove the responsibility of generating encryption

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,15 +1,18 @@
 import AppError from "@shared/errors/AppError";
-import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 import { ICreateUser } from "../domain/models/ICreateUser";
 import { IUser } from "../domain/models/IUser";
 import { IUsersRepository } from "../domain/repositories/IUserRepository";
+import { IHashProvider } from "../providers/HashProvider/models/IHashProvider";
 
 @injectable()
 class CreateUserService {
   constructor(
+    // Injetar dependências
     @inject("UserRepository")
     private userRepository: IUsersRepository,
+    @inject("HashProvider")
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ name, email, password }: ICreateUser): Promise<IUser> {
@@ -19,7 +22,7 @@ class CreateUserService {
       throw new AppError("Email address already used.");
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.userRepository.create({
       name,
