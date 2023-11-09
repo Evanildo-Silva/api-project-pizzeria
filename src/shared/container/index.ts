5c3d2ee0269b272ce7c9a38ab1f7eecf65ae8a45
evanildo <evanildo.sf91@gmail.com>
2023-11-09 19:19:58 -0300

    fix: import

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,6 @@
 import { IUsersRepository } from "@modules/users/domain/repositories/IUserRepository";
 import UserRepository from "@modules/users/infra/typeorm/repositories/UserRepository";
-import "@modules/users/providers"; // Import do container do HashProvider
+import "@modules/users/providers/HashProvider"; // Import do container do HashProvider
 import { container } from "tsyringe";
 // Config container para que esteja disponível para injetar nas classes
 container.registerSingleton<IUsersRepository>("UserRepository", UserRepository);
