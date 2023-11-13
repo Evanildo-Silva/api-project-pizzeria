a37e13ee619ce1a7bea958ab4634df8978f5eccd
evanildo <evanildo.sf91@gmail.com>
2023-11-13 17:25:08 -0300

    feat: add findall method

--- src/modules/categories/infra/typeorm/repositories/CategoryRepository.ts
+++ src/modules/categories/infra/typeorm/repositories/CategoryRepository.ts
@@ -1,9 +1,11 @@
+import { ICategory } from "@modules/categories/domain/models/ICategory";
 import { ICreateCategory } from "@modules/categories/domain/models/ICreateCategory";
+import { ICategoryRepository } from "@modules/categories/domain/repositories/ICategoryRepository";
 import { dataSource } from "@shared/infra/typeorm";
 import { Repository } from "typeorm";
 import Category from "../entities/category";
 
-class CategoryRepository {
+class CategoryRepository implements ICategoryRepository {
   private ormRepository: Repository<Category>;
 
   constructor() {
@@ -17,4 +19,12 @@ class CategoryRepository {
 
     return category;
   }
+
+  public async findAll(): Promise<ICategory[]> {
+    const categories = this.ormRepository.find();
+
+    return categories;
+  }
 }
+
+export default CategoryRepository;
