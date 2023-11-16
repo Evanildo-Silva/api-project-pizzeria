d55c81edad8112b3daae46fa8e93d73e798deb37
evanildo <evanildo.sf91@gmail.com>
2023-11-16 19:09:05 -0300

    feat: product controller

---
+++ src/modules/products/infra/http/controllers/ProductController.ts
@@ -0,0 +1,24 @@
+import CreateProductService from "@modules/products/services/CreateProductService";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+class ProductController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, description, category_id } = request.body;
+    const banner = request.file!.filename;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      description,
+      banner,
+      category_id,
+    });
+
+    return response.status(201).json(product);
+  }
+}
+
+export default ProductController;
