4879b3667a095ade849888eb12d4aff553957e4a
evanildo <evanildo.sf91@gmail.com>
2023-11-16 23:14:19 -0300

    feat: product creation route

---
+++ src/modules/products/infra/http/routes/product.routes.ts
@@ -0,0 +1,27 @@
+import uploadConfig from "@config/upload";
+import isAuthenticated from "@shared/infra/http/middleware/isAuthenticated";
+import { Joi, Segments, celebrate } from "celebrate";
+import { Router } from "express";
+import multer from "multer";
+import ProductController from "../controllers/ProductController";
+
+const productRouter = Router();
+const productController = new ProductController();
+const upload = multer(uploadConfig.upload("./temp"));
+
+productRouter.post(
+  "/register",
+  isAuthenticated,
+  upload.single("banner"),
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      description: Joi.string().required(),
+      category: Joi.string().required(),
+    },
+  }),
+  productController.create,
+);
+
+export default productRouter;
