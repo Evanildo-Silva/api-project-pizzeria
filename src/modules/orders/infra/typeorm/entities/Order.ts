35399ac3f8d0f56a04a0f0817633c7a9a0f0a5ce
evanildo <evanildo.sf91@gmail.com>
2023-11-20 20:52:12 -0300

    feat: order entities

---
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -0,0 +1,35 @@
+import { Item } from "@modules/items/infra/typeorm/entities/Item";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("orders")
+class Order {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  table: number;
+
+  @Column({ default: false })
+  status: boolean;
+
+  @Column({ default: true })
+  draft: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => Item, item => item.order)
+  items: Item[];
+}
+
+export default Order;
