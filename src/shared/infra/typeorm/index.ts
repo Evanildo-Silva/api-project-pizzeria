5c04c9411461b84ede457c200add8ae23f2a2db2
evanildo <evanildo.sf91@gmail.com>
2023-11-20 20:52:54 -0300

    feat: add migrations and entities

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,16 @@
 import Category from "@modules/categories/infra/typeorm/entities/Category";
+import { Item } from "@modules/items/infra/typeorm/entities/Item";
+import Order from "@modules/orders/infra/typeorm/entities/Order";
 import Product from "@modules/products/infra/typeorm/entities/Product";
 import User from "@modules/users/infra/typeorm/entities/User";
 import { DataSource } from "typeorm";
 import { CreateUsers1699408921898 } from "./migrations/1699408921898-CreateUsers";
 import { CreateCategories1699901605293 } from "./migrations/1699901605293-CreateCategories";
 import { CreateProducts1699916290430 } from "./migrations/1699916290430-CreateProducts";
+import { CreateOrders1700521375933 } from "./migrations/1700521375933-CreateOrders";
+import { CreateItems1700521940644 } from "./migrations/1700521940644-CreateItems";
+import { AddOrderIdToItems1700522613355 } from "./migrations/1700522613355-AddOrderIdToItems";
+import { AddProductIdToItems1700523182224 } from "./migrations/1700523182224-AddProductIdToItems";
 
 export const dataSource = new DataSource({
   type: "postgres",
@@ -13,10 +19,14 @@ export const dataSource = new DataSource({
   username: "postgres",
   password: "docker",
   database: "apipizzeria",
-  entities: [User, Category, Product],
+  entities: [User, Category, Product, Order, Item],
   migrations: [
     CreateUsers1699408921898,
     CreateCategories1699901605293,
     CreateProducts1699916290430,
+    CreateOrders1700521375933,
+    CreateItems1700521940644,
+    AddOrderIdToItems1700522613355,
+    AddProductIdToItems1700523182224,
   ],
 });
