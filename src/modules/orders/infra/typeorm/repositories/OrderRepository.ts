1362c277bad331656eeb4a7da0ea8293df8fd1c0
evanildo <evanildo.sf91@gmail.com>
2023-11-21 14:16:43 -0300

    feat: order repository

---
+++ src/modules/orders/infra/typeorm/repositories/OrderRepository.ts
@@ -0,0 +1,27 @@
+import { ICreateOrder } from "@modules/orders/domain/models/ICreateOrder";
+import { IOrder } from "@modules/orders/domain/models/IOrder";
+import { IOrderRepository } from "@modules/orders/domain/repositories/IOrderRepository";
+import { dataSource } from "@shared/infra/typeorm";
+import { Repository } from "typeorm";
+import Order from "../entities/Order";
+
+class OrderRepository implements IOrderRepository {
+  private ormRepository: Repository<Order>;
+
+  constructor() {
+    // Atribuir a instância do repositório desejado
+    this.ormRepository = dataSource.getRepository(Order);
+  }
+  public async create({ ...rest }: ICreateOrder): Promise<IOrder> {
+    const order = this.ormRepository.create({ ...rest });
+
+    await this.ormRepository.save(order);
+
+    return order;
+  }
+  public async findById(id: string): Promise<IOrder | null> {
+    const order = await this.ormRepository.findOneBy({ id });
+
+    return order;
+  }
+}
