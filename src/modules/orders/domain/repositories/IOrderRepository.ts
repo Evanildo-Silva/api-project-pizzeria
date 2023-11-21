a84169c18e3dae332acc9b74914fb4a99cbe63fc
evanildo <evanildo.sf91@gmail.com>
2023-11-21 20:01:52 -0300

    feat: add order send method in order repository interface

--- src/modules/orders/domain/repositories/IOrderRepository.ts
+++ src/modules/orders/domain/repositories/IOrderRepository.ts
@@ -1,7 +1,9 @@
+import Order from "@modules/orders/infra/typeorm/entities/Order";
 import { ICreateOrder } from "../models/ICreateOrder";
 import { IOrder } from "../models/IOrder";
 
 export interface IOrderRepository {
   create(data: ICreateOrder): Promise<IOrder>;
   findById(id: string): Promise<IOrder | null>;
+  sendOrder(id: string): Promise<Order | null>;
 }
