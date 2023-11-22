67033bca9005ed05ad362ddef95c0889ac3ff9ba
evanildo <evanildo.sf91@gmail.com>
2023-11-22 20:15:18 -0300

    feat: add methods interface

--- src/modules/items/domain/repositories/IItemRepository.ts
+++ src/modules/items/domain/repositories/IItemRepository.ts
@@ -3,4 +3,6 @@ import { IItem } from "../models/IItem";
 
 export interface IItemRepository {
   create({ order_id, product_id, amount }: ICreateItem): Promise<IItem>;
+  findById(id: string): Promise<IItem | null>;
+  remove(item: IItem): Promise<void>;
 }
