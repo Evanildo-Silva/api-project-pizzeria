e86c74b0b5d05ef1f95554a18dc70b3384ad7ced
evanildo <evanildo.sf91@gmail.com>
2023-11-21 22:24:11 -0300

    feat: add items property in order interface

--- src/modules/orders/domain/models/IOrder.ts
+++ src/modules/orders/domain/models/IOrder.ts
@@ -1,9 +1,12 @@
+import { Item } from "@modules/items/infra/typeorm/entities/Item";
+
 export interface IOrder {
   id: string;
   table: number;
   status: boolean;
   draft: boolean;
   name?: string;
+  items: Item[];
   created_at: Date;
   updated_at: Date;
 }
