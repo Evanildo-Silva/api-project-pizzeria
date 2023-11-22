1a2319fabbc521ab5bfddb1fe3a0e53cbb0b7d6b
evanildo <evanildo.sf91@gmail.com>
2023-11-21 21:52:47 -0300

    refactor: typing return

--- src/modules/orders/services/FindConfirmedOrdersService.ts
+++ src/modules/orders/services/FindConfirmedOrdersService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { IOrder } from "../domain/models/IOrder";
 import { IOrderRepository } from "../domain/repositories/IOrderRepository";
 
 @injectable()
@@ -8,7 +9,7 @@ class FindConfirmedOrdersService {
     private orderRepository: IOrderRepository,
   ) {}
 
-  public async execute() {
+  public async execute(): Promise<IOrder[] | null> {
     const confirmedOrders = await this.orderRepository.findConfirmedOrders();
 
     return confirmedOrders;
