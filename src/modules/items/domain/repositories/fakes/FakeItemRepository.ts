8e8d7ef23423de94cc532e3d579c041174f03ae6
evanildo <evanildo.sf91@gmail.com>
2023-11-22 20:32:00 -0300

    feat: fake item repository

---
+++ src/modules/items/domain/repositories/fakes/FakeItemRepository.ts
@@ -0,0 +1,39 @@
+import { Item } from "@modules/items/infra/typeorm/entities/Item";
+import { v4 as uuidv4 } from "uuid";
+import { ICreateItem } from "../../models/ICreateItem";
+import { IItem } from "../../models/IItem";
+import { IItemRepository } from "../IItemRepository";
+
+class FakeItemRepository implements IItemRepository {
+  private items: Item[] = [];
+  public async create({
+    order_id,
+    product_id,
+    amount,
+  }: ICreateItem): Promise<Item> {
+    const item = new Item();
+
+    item.id = uuidv4();
+    item.amount = amount;
+    item.order_id = order_id;
+    item.product_id = product_id;
+
+    this.items.push(item);
+
+    return item;
+  }
+  public async findById(id: string): Promise<IItem | null> {
+    const item = this.items.find(item => item.id === id);
+
+    if (item) {
+      return item;
+    }
+
+    return null;
+  }
+  public async remove(item: IItem): Promise<void> {
+    this.items.filter(data => data !== item);
+  }
+}
+
+export default FakeItemRepository;
