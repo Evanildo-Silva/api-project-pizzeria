1223772ff83bf1805fae31b6eb02deb6d90332d7
evanildo <evanildo.sf91@gmail.com>
2023-11-22 21:51:17 -0300

    feat: delete item test

---
+++ src/modules/items/services/DeleteItemService.spec.ts
@@ -0,0 +1,30 @@
+import AppError from "@shared/errors/AppError";
+import Mock from "../domain/mocks/mockTestItem";
+import FakeItemRepository from "../domain/repositories/fakes/FakeItemRepository";
+import DeleteItemService from "./DeleteItemService";
+
+let fakeItemRepository: FakeItemRepository;
+let deleteItemService: DeleteItemService;
+
+describe("DeleteItem", () => {
+  beforeEach(() => {
+    fakeItemRepository = new FakeItemRepository();
+    deleteItemService = new DeleteItemService(fakeItemRepository);
+  });
+
+  it("should be able to return the list without the item informed by the id", async () => {
+    const item = await fakeItemRepository.create({
+      amount: 1,
+      order_id: Mock.order,
+      product_id: Mock.product,
+    });
+
+    // TODO verifica como buscar o retorno
+
+    await deleteItemService.execute(item.id);
+  });
+
+  it("should be able to return an erro", async () => {
+    expect(deleteItemService.execute("")).rejects.toBeInstanceOf(AppError);
+  });
+});
