e2edc55bf811b76091033855aeee7bea26bdef67
evanildo <evanildo.sf91@gmail.com>
2023-11-22 22:18:19 -0300

    feat: item controller

---
+++ src/modules/items/infra/http/controllers/ItemController.ts
@@ -0,0 +1,32 @@
+import CreateItemService from "@modules/items/services/CreateItemService";
+import DeleteItemService from "@modules/items/services/DeleteItemService";
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+class ItemController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { amount, order_id, product_id } = request.body;
+
+    const createItem = container.resolve(CreateItemService);
+
+    const item = await createItem.execute({
+      amount,
+      order_id,
+      product_id,
+    });
+
+    return response.json(item);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const id = request.query.id as string;
+
+    const deleteItem = container.resolve(DeleteItemService);
+
+    await deleteItem.execute(id);
+
+    return response.status(204).json();
+  }
+}
+
+export default ItemController;
