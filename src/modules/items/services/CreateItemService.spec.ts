3b4da60cc1b97e962adf6e96da88d5a82ebcd524
evanildo <evanildo.sf91@gmail.com>
2023-11-22 21:52:31 -0300

    refactor: add mock for testing items

--- src/modules/items/services/CreateItemService.spec.ts
+++ src/modules/items/services/CreateItemService.spec.ts
@@ -1,37 +1,10 @@
 import AppError from "@shared/errors/AppError";
+import Mock from "../domain/mocks/mockTestItem";
 import FakeItemRepository from "../domain/repositories/fakes/FakeItemRepository";
 import CreateItemService from "./CreateItemService";
 
 let fakeItemRepository: FakeItemRepository;
 let createItem: CreateItemService;
-
-const order = {
-  id: "55129b14-2000-41f0-9381-0431545a2d24",
-  table: 58,
-  status: false,
-  draft: false,
-  created_at: new Date(),
-  updated_at: new Date(),
-  items: [],
-};
-const category = {
-  id: "e9c87eaf-6bbd-4e19-88f9-a6889d630f2e",
-  name: "Bebidas",
-  created_at: new Date(),
-  updated_at: new Date(),
-  products: [],
-};
-const product = {
-  id: "ff9a5466-2990-4085-a5b4-d1fd223122b2",
-  name: "sprite",
-  price: 5.9,
-  description: "Refrigerante sabor Cola, 350ml.",
-  banner: "63df88d770034d3d9bde1fdde9ce56ca-stripe-lata.png",
-  created_at: new Date(),
-  updated_at: new Date(),
-  items: [],
-  category: category,
-};
 
 describe("CreateItem", () => {
   beforeEach(() => {
@@ -42,8 +15,8 @@ describe("CreateItem", () => {
   it("should be able create a new item", async () => {
     const item = await createItem.execute({
       amount: 2,
-      order_id: order,
-      product_id: product,
+      order_id: Mock.order,
+      product_id: Mock.product,
     });
 
     expect(item).toHaveProperty("id");
@@ -53,8 +26,8 @@ describe("CreateItem", () => {
     expect(
       createItem.execute({
         amount: 0,
-        order_id: order,
-        product_id: product,
+        order_id: Mock.order,
+        product_id: Mock.product,
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
