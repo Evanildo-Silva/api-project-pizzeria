952d8da7eec04e1f45e428bc07525e16a946015a
evanildo <evanildo.sf91@gmail.com>
2023-11-27 21:55:51 -0300

    fix: complete test

--- src/modules/products/services/CreateProductService.spec.ts
+++ src/modules/products/services/CreateProductService.spec.ts
@@ -29,7 +29,7 @@ describe("CreateProduct", () => {
     expect(product).toHaveProperty("id");
   });
 
-  it("should not be able to create a new product", async () => {
+  it("should not be able to create a new product with the same name", async () => {
     await createProduct.execute({
       name: "Produto teste",
       price: 10.0,
@@ -50,6 +50,24 @@ describe("CreateProduct", () => {
         price: 10.0,
         description: "Produto criado para testes",
         banner: "image",
+        category: {
+          id: "7e6354c1-8e22-4b59-953d-752b03b4076b",
+          name: "Categoria teste",
+          products: [],
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("shouldn't be able to create a new product without an image", async () => {
+    expect(
+      createProduct.execute({
+        name: "Produto teste",
+        price: 10.0,
+        description: "Produto criado para testes",
+        banner: "",
         category: {
           id: "7e6354c1-8e22-4b59-953d-752b03b4076b",
           name: "Categoria teste",
